8eae35c55b38031cc41499cc364d90ce91c505a9
=Islam Hassan <islam.abuaish@gmail.com>
2022-03-28 19:25:22 +0300

    edits

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ var app = express();
 var PORT = 3000;
  
 app.get('/', (req, res) => {
-  res.send("GET  : IP :: " +ip.address() ) ;
+  res.send("IP :" +ip.address() ) ;
 })
 
 app.get('/title', (req, res) => {
   res.send("Test Code") ;
 })
- 
+
+
+app.get('/employees', (req, res) => {
+  res.send(dsdsdsd) ;
+})
+
+
+
+
  
 app.listen(PORT, function(err){
     if (err) console.log(err);
